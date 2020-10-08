757b8c6df6fae4587e37f9bdc123b8b46438ad3e
Morgan Houston <morgan.houston@okstate.edu>
2020-10-07 19:34:50 -0500

    commit

---
+++ Assets/Challenge 1/Scripts/RotatePropellerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class RotatePropellerX : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rotates propeller
+        transform.Rotate(0.0f, 0.0f, 25.0f, Space.Self);
+    }
+}
---
+++ Assets/ReadMe.txt
@@ -0,0 +1,9 @@
+Morgan Houston
+08/29/2020
+
+Made plane go forward.
+Slowed plane down.
+Plane only tilts on user input of up/down arrows.
+Repositioned camera to the side of plane.
+Made camera follow plane.
+Made propeller spin!
